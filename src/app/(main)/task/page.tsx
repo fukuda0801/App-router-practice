5a5dfe748f82970bd575cab0be6f4b810168c58f
fukuda0801 <nagisano0801@gmail.com>
2024-10-10 15:48:37 +0900

    feat: ページ内でapiへリクエストを送り、データを取得する

--- src/app/(main)/task/page.tsx
+++ src/app/(main)/task/page.tsx
@@ -1,9 +1,24 @@
-const TaskPage = () => {
+import { Task } from "@/app/api/tasks/route";
+
+const getTasks = async () => {
+  const response = await fetch("http://localhost:3000/api/tasks", {
+    method: "GET",
+  });
+  return await response.json();
+};
+
+const page = async () => {
+  const tasks = (await getTasks()).tasks as Task[];
   return (
     <div>
-      <p>TaskPage</p>
+      <div>TaskPage</div>
+      <div>
+        {tasks.map((task) => {
+          return <div key={task.id}>{task.name}</div>;
+        })}
+      </div>
     </div>
   );
 };
 
-export default TaskPage;
+export default page;
